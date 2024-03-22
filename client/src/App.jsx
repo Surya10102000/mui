2573919a791730dc6e2c95571fae66b24da76825
Surya Prakash Mallah <sfrost488@gmail.com>
2024-03-22 21:26:48 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <>
+      helloworld
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "FF71CD",
+      
+      
+    },
+    secondary: {
+      main: '#5755FE',
+      light: '#F5EBFF',
+      contrastText: '#47008F',
+    },
+    other: {
+        main : "8B93FF"
+    }
+  },
+});
+
+export default theme
